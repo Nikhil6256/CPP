d372be08deae5b6f5fc94cc3bfc3583813d582af
Nikhil6256 <182553411+Nikhil6256@users.noreply.github.com>
2024-11-25 19:14:48 +0530

    jjj

--- 1st.cpp
+++ 1st.cpp
@@ -1,1 +1,11 @@
-hiiii
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Mai Nikhil Hu"<<endl;
+    cout<<"Mai Nikhil Hu"<< '\n';
+    cout<<"Mai Nikhil Hu\n";
+    cout<<"Mai Nikhil Hu";
+    cout<<"\n";
+    cout<<"Mai Nikhil Hu";
+}
