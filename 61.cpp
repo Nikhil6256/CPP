2f77dfb20c6dbedf41e611eb2c6035e2d4d7122d
Nikhil6256 <182553411+Nikhil6256@users.noreply.github.com>
2024-12-05 20:49:19 +0530

    using exit function in infinite while loop

---
+++ 61.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=1,i;
+    while(1){
+        cout<<"Hi\n";
+        switch(n){
+            case 1 : cout<<"First\n";
+            break;
+        }
+        i++;
+        exit(1);
+    }
+}
+
