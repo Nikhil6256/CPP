f9f7d26a03f30edd037568481e730c9e2ef1905c
Nikhil6256 <182553411+Nikhil6256@users.noreply.github.com>
2024-12-05 20:55:57 +0530

    using exit fun. in infinite while loop

--- 61.cpp
+++ 61.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
+#include <cstdlib> // Required for exit()
 using namespace std;
 int main(){
-    int n=1,i;
+    int n=1,i=0;
     while(1){
         cout<<"Hi\n";
         switch(n){
             case 1 : cout<<"First\n";
-            break;
+            exit(0);
         }
         i++;
-        exit(1);
     }
 }
 
