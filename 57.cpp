a387df49f90d668746c9f7a95637b0632331caa5
Nikhil6256 <182553411+Nikhil6256@users.noreply.github.com>
2024-12-05 18:01:13 +0530

    string use

---
+++ 57.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string name1,name2;
+    int percentage1,percentage2;
+    cin >> name1 >> name2;
+    cin>>percentage1>>percentage2;
+
+    if(percentage1>percentage2){
+        cout<<name1;
+    }
+    else if(percentage2>percentage1){
+        cout<<name2;
+    }
+    else{
+        cout<<"Equal\n";
+    }
+}
