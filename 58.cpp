43494306116dea99c06d8f3cd745b473e171832b
Nikhil6256 <182553411+Nikhil6256@users.noreply.github.com>
2024-12-05 19:10:41 +0530

    switch case using int

---
+++ 58.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num=2;
+    switch(num){
+        case 1: cout<<"First\n";
+        break;
+
+        case 2: cout<<"Second\n";
+        break;
+
+        default: cout<<"It is default case\n";
+    }
+}
