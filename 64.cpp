84682dd29ab2cb9ec48e9eb1938fe430c8a0e8f2
Nikhil6256 <182553411+Nikhil6256@users.noreply.github.com>
2024-12-11 01:23:17 +0530

    First function program

--- 64.cpp
+++ 64.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int power(){
-    int a,b;
-    cin>>a>>b;
+int power(int num1, int num2){
 
     int ans = 1;
-    for(int i = 1; i<=b;i++){
-        ans = ans * a;
+    for(int i = 1; i<=num2;i++){
+        ans = ans * num1;
     }
     return ans;
 }
 
 int main(){
-    int ans = power();
-    cout<<"answer is "<< ans <<endl;
+    int a,b;
+    cin>>a>>b;
+    int answer = power(a,b);
+    cout<<"answer is "<< answer <<endl;
                 
     return 0;
 }
